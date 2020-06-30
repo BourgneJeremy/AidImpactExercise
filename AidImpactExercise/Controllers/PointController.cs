198bf1d08ca91980dccd7fce49097bb5fa367bc4
Jeremy Bourgne <jeremy.bourgne.louis@gmail.com>
2020-06-30 15:57:04 +0200

    add creation of a point

---
+++ AidImpactExercise/Controllers/PointController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AidImpactExercise.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AidImpactExercise.Controllers
+{
+    [Route("point")]
+    public class PointController : Controller
+    {
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Post(Point point)
+        {
+            return View(point);
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Point point)
+        {
+            // If isn't valid get back to basic view
+            if (!ModelState.IsValid)
+                return View();
+
+            return RedirectToAction("Post", "Point", point);
+        }
+
+        /*public IActionResult Modify()
+        {
+            return View();
+        }*/
+    }
+}
---
+++ AidImpactExercise/Models/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AidImpactExercise.Models
+{
+    public class Point
+    {
+        [Display(Name = "City name")]
+        [Required]
+        [StringLength(50, MinimumLength = 3,
+            ErrorMessage = "The name of the city must contains between 3 and 50 caracters")]
+        public string Name { get; set; }
+
+        [Display(Name = "City longitude")]
+        [Required]
+        public double Longitude { get; set; }
+
+        [Display(Name = "City latitude")]
+        [Required]
+        public double Latitude { get; set; }
+    }
+}
